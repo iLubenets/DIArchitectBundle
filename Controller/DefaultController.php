849987f4bbee11a21a81ad6a5784e27779c41d5c
Igor Lubenets <iLubenets@gmail.com>
2014-10-23 00:26:13 +0300

    Configuration bundle key changed

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,13 +2,14 @@
 
 namespace iLubenets\DIArchitectBundle\Controller;
 
+use iLubenets\DIArchitectBundle\DependencyInjection\Configuration;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function diAction($bundle_name)
     {
-        $pathToJson = $this->container->getParameter('di_architect.path_to_save_json');
+        $pathToJson = $this->container->getParameter(Configuration::BUNDLE.'.path_to_save_json');
         $pathToJson = str_replace('web/', '', $pathToJson);
         return $this->render(
             'iLubenetsDIArchitectBundle:Charts:code_structure.html.twig',
